965dc90276b9535af7af2384ad43a133d655265f
Diogo Dias <diogodiasgrilo@gmail.com>
2023-05-04 23:47:10 +0100

    Update to the header files 23:47, May 4

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 11:30:49 by diogpere          #+#    #+#             */
-/*   Updated: 2023/05/04 22:19:03 by diogpere         ###   ########.fr       */
+/*   Updated: 2023/05/04 23:46:00 by diogpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ int	free_map_exit(void *game)
 	exit(0);
 }
 
-void	manage_cleanup(t_game *g, int x, int y)
+int	manage_cleanup(int key, t_game *g, int x, int y)
 {
+	if (key == 65307)
+		free_map_exit(g);
 	if (g->map[g->robin_y][g->robin_x] == 'E' && g->n_collect == 0)
 	{
 		printf("You Win!\n");
@@ -47,29 +49,35 @@ void	manage_cleanup(t_game *g, int x, int y)
 		printf("Current N. of Moves -> %d\n", g->n_moves);
 	}
 	new_game_image(*g, g->map);
+	return (0);
 }
 
 int	ft_input(int key, void *param)
 {
 	t_game	*g;
-	int		x;
-	int		y;
+	int		size;
 
+	size = SIZE;
 	g = (t_game *)param;
-	x = g->robin_x;
-	y = g->robin_y;
+	g->x_comp = g->robin_x;
+	g->y_comp = g->robin_y;
 	if (key == 100 && g->map[g->robin_y][g->robin_x + 1] != '1')
+	{
+		g->sprites.robin = mlx_xpm_file_to_image(g->id, \
+			"./sprites/robin.xpm", &size, &size);
 		g->robin_x += 1;
+	}
 	else if (key == 97 && g->map[g->robin_y][g->robin_x - 1] != '1')
+	{
+		g->sprites.robin = mlx_xpm_file_to_image(g->id, \
+			"./sprites/robin_left.xpm", &size, &size);
 		g->robin_x -= 1;
+	}
 	else if (key == 115 && g->map[g->robin_y + 1][g->robin_x] != '1')
 		g->robin_y += 1;
 	else if (key == 119 && g->map[g->robin_y - 1][g->robin_x] != '1')
 		g->robin_y -= 1;
-	else if (key == 65307)
-		free_map_exit(g);
-	manage_cleanup(g, x, y);
-	return (0);
+	return (manage_cleanup(key, g, g->x_comp, g->y_comp));
 }
 
 int	ft_update(void *param)
